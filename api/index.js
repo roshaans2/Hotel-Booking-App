209c01b31afc8c2501820d67b85fbc3a326e2c09
roshaans2 <roshaansathya@gmail.com>
2023-08-26 12:51:33 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+const MONGO_URI = "mongodb+srv://Roshaan:roshaan@roshaan.lwu23.mongodb.net/booking-app"
+
+const authRouter = require("./routes/auth.js")
+const hotelsRouter = require("./routes/hotels.js")
+const roomsRouter = require("./routes/rooms.js")
+const usersRouter = require("./routes/users.js")
+
+
+const connect = async ()=>{
+    try {
+      await mongoose.connect(MONGO_URI)
+      console.log("Connected to the DB")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+app.use("/api/auth",authRouter);
+app.use("/api/hotels",hotelsRouter)
+app.use("/api/rooms",roomsRouter)
+app.use("/api/users",usersRouter)
+
+
+app.listen(8080,()=>{
+    connect()
+    console.log("Connected to Backend!")
+})
