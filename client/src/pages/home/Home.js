034fb25d50ff5af50c94c2d9cd2d3d49e4e9d665
roshaans2 <roshaansathya@gmail.com>
2023-10-08 11:40:15 +0530

    Navbar and Header desing

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,8 +1,13 @@
 import './home.css'
+import Navbar from '../../components/navbar/Navbar'
+import Header from '../../components/header/Header'
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <>
+        <Navbar/>
+        <Header/>
+        </>
     )
 }
 
