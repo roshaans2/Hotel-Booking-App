cb53c4a14b6b9fd21def986d8e26c4b7cb1118ed
roshaans2 <roshaansathya@gmail.com>
2023-10-08 10:41:23 +0530

    Creating Router

---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,9 @@
+import './home.css'
+
+const Home = () => {
+    return (
+        <h1>Home</h1>
+    )
+}
+
+export default Home
---
+++ client/src/pages/hotel/Hotel.js
@@ -0,0 +1,7 @@
+const Hotel = () => {
+    return (
+        <h1>Hotel</h1>
+    )
+}
+
+export default Hotel;
